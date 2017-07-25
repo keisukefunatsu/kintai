2c4a62796fb352acba541d6b279e9957a21ae85d
funatsu <keisuekyan2012@gmail.com>
2017-07-25 19:01:06 +0900

    開発環境で毎日ユーザの出勤ステータスをリセットするタスクを追加

--- README.md
+++ README.md
@@ -21,4 +21,8 @@ Things you may want to cover:
 
 * Deployment instructions
 
+bundle install
+rails db:migrate
+whenever --update-crontab
+
 * ...
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :in_dates, dependent: :destroy
   has_many :out_dates, dependent: :destroy
   paginates_per 15
+
+  def self.reset_status
+    all.update_all(present: '3')
+  end
 end
